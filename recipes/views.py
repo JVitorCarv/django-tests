487ad0711779140c5db6f6499b6abe3b1a2c3de9
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-04-21 13:53:06 -0300

    Create partial for head

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,6 @@ from django.shortcuts import render
 # HTTP REQUEST
 def home(request):
     # o render entende que esse arquivo pode estar na pasta "templates", basta nao esquecer de adicionar o app criado no settings
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Testing name in context',
     })
